8a8777a57d8ea6085bece7047edfddb66bb8ae0f
marl0909 <taraskolesnyk0909@gmail.com>
2023-05-11 11:15:28 +0300

    ADD images

---
+++ src/assets/texts.js
@@ -0,0 +1,36 @@
+import doctor1 from './img/Doctor1.png'
+import doctor2 from './img/Doctor2.png'
+import doctor3 from './img/Doctor3.png'
+import doctor4 from './img/Doctor4.png'
+import doctor5 from './img/Doctor5.png'
+import doctor6 from './img/Doctor6.png'
+
+
+export const description = 'Our cardiovascular specialists are ready to help you with the treatment of various heart diseases. At our medical clinic, we use the most advanced methods of diagnosis and treatment to help you regain the health of your heart and blood vessels. We offer a wide range of services, including examination and diagnosis, treatment of arrhythmias, coronary heart disease, heart failure, as well as performing cardiovascular surgeries. Our team of specialists has extensive experience and constantly improves their skills to provide the most effective treatment for each patient. We are ready to answer your questions and provide you with individual consultation.'
+
+export const otherServices = [
+    {name: 'Echocardiogram:', lowPrice: 300, highPrice: 500},
+    {name: 'Stress test:', lowPrice: 200, highPrice: 400},
+    {name: 'Coronary angiography:', lowPrice: 3000, highPrice: 8000},
+    {name: 'Cardiac catheterization: ', lowPrice: 5000, highPrice: 15000},
+    {name: 'Angioplasty:', lowPrice: 5000, highPrice: 20000},
+    {name: 'Pacemaker implantation:', lowPrice: 20000, highPrice: 50000},
+    {name: 'Heart surgery:', lowPrice: 50000, highPrice: 200000},
+]
+
+export const popularServices = [
+    {name: 'Consultation with a cardiologist', lowPrice: 150, highPrice: 300},
+    {name: 'Holter monitoring', lowPrice: 200, highPrice: 400},
+    {name: 'ECG (Electrocardiogram)', lowPrice: 50, highPrice: 200},
+]
+
+
+export const doctors = [
+    {name: 'Dr. Emily Johnson', experience: 12, realm: 'cardiology', photo: doctor1 },
+    {name: 'Dr. Ben Parker', experience: 8, realm: 'internal medicine', photo: doctor2},
+    {name: 'Dr. Olivia Davis', experience: 10, realm: 'pulmonology', photo: doctor3},
+    {name: 'Dr. Tyler Mitchell', experience: 7, realm: 'gastroenterology', photo: doctor4},
+    {name: 'Dr. Samantha Green', experience: 15, realm: 'endocrinology', photo: doctor5},
+    {name: 'Dr. Nicholas Carter', experience: 6, realm: 'oncology', photo: doctor6},
+]
+
